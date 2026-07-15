d37e0cf2e590502f4ab10f18a91d631916b9e97a
junaid-shaukat <junaid.shaukat.dev@gmail.com>
2026-07-15 12:50:34 +0500

    fix: set basename for BrowserRouter to '/syaanh'

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
       <DirectionProvider>
-        <BrowserRouter>
+        <BrowserRouter basename="/syaanh">
           <App />
         </BrowserRouter>
       </DirectionProvider>
